579e65e50773bad7d9e891a93a1625dae55d4a70
Ricardo Alves da Silva <ricardcpu@gmail.com>
2026-03-05 22:34:34 -0400

    chore(setup): add commitlint and husky

---
+++ eslint.config.mjs
@@ -0,0 +1,36 @@
+import js from "@eslint/js";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import prettier from "eslint-config-prettier";
+
+export default [
+  {
+    ignores: [
+      "node_modules/**",
+      "dist/**",
+      "build/**",
+      ".expo/**",
+      "android/**",
+      "ios/**",
+      "commitlint.config.cjs",
+    ],
+  },
+  js.configs.recommended,
+  prettier,
+  {
+    files: ["**/*.{ts,tsx,js,jsx}"],
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+    },
+    rules: {
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "no-console": "off",
+    },
+  },
+];
